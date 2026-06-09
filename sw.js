201203f4362170a1ed93432a81b68f2a107c305c
amartiolivart <amartiolivart>
2026-06-09 12:55:17 +0200

    Fix news feed text clipping and player photos

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'matchday-v43';
+const CACHE_NAME = 'matchday-v44';
 const ASSETS = [
   './',
   './index.html',
-  './style.css?v=43',
-  './app.js?v=43',
+  './style.css?v=44',
+  './app.js?v=44',
   './manifest.json',
   './assets/icon-192.png',
   './assets/icon-512.png'
